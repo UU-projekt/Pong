e93e52278204829e587dc184416fc9c9cf924938
Leahandersen24 <105506054+Leahandersen24@users.noreply.github.com>
2025-10-22 10:31:43 +0200

    Update Ball.cs

--- GameObjects/Ball.cs
+++ GameObjects/Ball.cs
@@ -1,7 +1,37 @@
 public class Ball<T> : GameObject
 {
-    public Ball(uint width, uint height) : base(width, height)
-    {
+    public int VelocityX { get; set; }
+    public int VelocityY { get; set; }
 
+    public Ball(uint width, uint height, int velocityX, int velocityY) 
+        : base(width, height)
+    {
+        VelocityX = velocityX;
+        VelocityY = velocityY;
+    }
+
+    public void Move()
+    {
+        TranslateClamped(VelocityX, VelocityY);
+    }
+
+    public void BounceVertical()
+    {
+        VelocityY = -VelocityY;
+    }
+
+    public void BounceHorizontal()
+    {
+        VelocityX = -VelocityX;
+    }
+
+    public void Draw()
+    {
+        Console.SetCursorPosition((int)Position.X, (int)Position.Y);
+        Console.Write("O");
     }
 }
+
+
+public class StandardBall { }
+public class FastBall { }
