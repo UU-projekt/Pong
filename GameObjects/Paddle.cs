bbba3e29b973c54dad1fa0288d96d45849a8f0cd
Leahandersen24 <105506054+Leahandersen24@users.noreply.github.com>
2025-10-23 09:39:29 +0200

    Update Paddle.cs

--- GameObjects/Paddle.cs
+++ GameObjects/Paddle.cs
@@ -1,7 +1,36 @@
-public class Paddle<T> : GameObject where T : IPaddleController
+public class Paddle : GameObject
 {
-    public Paddle(uint width, uint height) : base(width, height)
-    {
+    private IPaddleController controller;
 
+    public Paddle(uint width, uint height, IPaddleController controller) 
+        : base(width, height)
+    {
+        this.controller = controller;
+    }
+
+    public void UpdateController(int ballY, int paddleY, int paddleHeight)
+    {
+        controller.Update(ballY, paddleY, paddleHeight);
+    }
+
+    public void Move()
+    {
+        int move = controller.GetMove();
+        if (move != 0)
+        {
+            TranslateClamped(0, move);
+        }
+    }
+
+    public void Draw()
+    {
+        for (uint i = 0; i < Size.Height; i++)
+        {
+            Console.SetCursorPosition(
+                (int)Position.X, 
+                (int)(Position.Y + i)
+            );
+            Console.Write("|");
+        }
     }
 }
