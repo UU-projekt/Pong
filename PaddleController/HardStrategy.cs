86f0eff73c52da1d9748893b445621f53d297293
Leahandersen24 <105506054+Leahandersen24@users.noreply.github.com>
2025-10-25 10:25:07 +0200

    Update HardStrategy.cs

--- PaddleController/HardStrategy.cs
+++ PaddleController/HardStrategy.cs
@@ -1,1 +1,14 @@
 
+public class HardStrategy : IDifficultyStrategy
+{
+    public int CalculateMove(int ballY, int paddleCenter)
+    {
+        // Svår: Perfekt följning av bollen
+        if (ballY < paddleCenter)
+            return -1;
+        else if (ballY > paddleCenter)
+            return 1;
+
+        return 0;
+    }
+}
